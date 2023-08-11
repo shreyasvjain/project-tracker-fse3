e8ee992d51a58b55e06db2ec8bf626da43678969
shreyasvjain <136169339+shreyasvjain@users.noreply.github.com>
2023-08-11 19:14:41 +0530

    Add files via upload

--- fse3/src/main/java/com/cognizant/fse3/Fse3Application.java
+++ fse3/src/main/java/com/cognizant/fse3/Fse3Application.java
@@ -2,8 +2,10 @@ package com.cognizant.fse3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Fse3Application {
 
 	public static void main(String[] args) {
